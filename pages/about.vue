9a30b209a7ebd4bd02f6a6e93fbf61fc03fe1f16
rizkygustianto <rizkyrebellion@gmail.com>
2020-12-22 16:11:37 +0700

    for deployment

---
+++ pages/about.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <the-title titlestring='About Us'/>
+    <about-content />
+    
+  </div>
+</template>
+
+<script>
+import TheTitle from '@/components/TheTitle'
+import Features from '@/components/Features'
+import Teasers from '@/components/Teasers'
+import Prices from '@/components/Prices'
+import CallToAction from '@/components/CallToAction'
+import AboutContent from '@/components/AboutContent.vue'
+
+export default {
+  name: 'About',
+  components: {
+    'the-title': TheTitle,
+    'about-content': AboutContent,
+    teasers: Teasers,
+    prices: Prices,
+    'call-to-action': CallToAction
+  }
+}
+</script>
---
+++ pages/blog.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <the-title titlestring='Journal'/>
+    <!-- <about-content /> -->
+   <Blogs />
+    
+  </div>
+</template>
+
+<script>
+import Blogs from '@/components/Blogs'
+import TheTitle from '@/components/TheTitle'
+
+export default {
+  name: 'Blog',
+  components: {
+    Blogs,
+    'the-title': TheTitle
+  }
+}
+</script>
